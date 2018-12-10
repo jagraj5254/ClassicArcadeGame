cff4de8ce59c257bd0f0ad7dbd03c810b3cd064e
Jagraj Singh <jagraj.khera0001@gmail.com>
2018-12-10 10:16:37 -0600

    "images are optimized"

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer')
 const cleanCSS = require('gulp-clean-css')
+const imagemin = require('gulp-imagemin')
 
-gulp.task('default', ['css'], function(){
-    gulp.watch('./src/css/**/*/.css', ['css']);
+gulp.task('default', ['css', 'images'], function(){
+    gulp.watch('./src/css/**/*.css', ['css']);
 });
 
 gulp.task('css', function(){
-    return gulp.src('./src/css/**/*/.css')
+    return gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer({browsers: ['last 2 versions']}))
         .pipe(cleanCSS())
         .pipe(gulp.dest('./dist/css'))
 });
+
+gulp.task('images', function(){
+    return gulp.src('./src/images/**/*.png')
+        .pipe(imagemin({verbose: true}))
+        .pipe(gulp.dest('./dist/images'))
+})
