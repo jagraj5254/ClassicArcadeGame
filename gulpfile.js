140ce9c63d17a4b1afe4cb7e396b167b0234cc36
Jagraj Singh <jagraj.khera0001@gmail.com>
2018-12-10 09:47:11 -0600

    "css minified"

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
 
 gulp.task('default', ['css'], function(){
     gulp.watch('./src/css/**/*/.css', ['css']);
@@ -8,5 +9,6 @@ gulp.task('default', ['css'], function(){
 gulp.task('css', function(){
     return gulp.src('./src/css/**/*/.css')
         .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./dist/css'))
 });
